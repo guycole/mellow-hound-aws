8c122bf9b0e166bc1cc2ed815f3068eed90da674
Guy Cole <guycole@gmail.com>
2016-08-10 15:52:44 -0700

    improved exception handling

--- src/main/python/mellow_hound/loader_v1.py
+++ src/main/python/mellow_hound/loader_v1.py
@@ -76,19 +76,19 @@ class LoaderVersion1:
 
             if raw_scan.endswith(','):
                 raw_scan = raw_scan[:len(raw_scan)-1]
+
+            fresh = Ble(observation_id, address, name, rssi, raw_scan)
+            session.add(fresh)
+            session.commit()
+
+            self.ble_loaded += 1
+
+            return fresh
         except:
-            print 'ble parse failure'
+            self.log_writer(6, 'ble failure', task_id, session)
             return
 
-        fresh = Ble(observation_id, address, name, rssi, raw_scan)
-        session.add(fresh)
-        session.commit()
-
-        self.ble_loaded += 1
-
-        return fresh
-
-    def process_wifi(self, observation_id, parsed_json, session):
+    def process_wifi(self, observation_id, parsed_json, task_id, session):
         try:
             ssid = parsed_json['ssid']
             if len(ssid) < 1:
@@ -97,186 +97,141 @@ class LoaderVersion1:
             capability = parsed_json['capability']
             frequency = parsed_json['frequency']
             level = parsed_json['level']
+
+            fresh = WiFi(observation_id, frequency, level, ssid, bssid, capability)
+
+            session.add(fresh)
+            session.commit()
+
+            self.wifi_loaded += 1
+
+            return fresh
         except:
-            print 'wifi parse failure'
+            self.log_writer(6, 'wifi failure', task_id, session)
             return
 
-        fresh = WiFi(observation_id, frequency, level, ssid, bssid, capability)
-        session.add(fresh)
-        session.commit()
-
-        self.wifi_loaded += 1
-
-        return fresh
-
-    def process_cdma(self, observation_id, parsed_json, session):
-        try:
-            register_flag = parsed_json['registerFlag']
-            base_station = parsed_json['baseStation']
-            latitude = parsed_json['latitude']
-            longitude = parsed_json['longitude']
-            network = parsed_json['network']
-            system = parsed_json['system']
-            asu_level = parsed_json['asuLevel']
-            cdma_dbm = parsed_json['cdmaDbm']
-            cdma_ecio = parsed_json['cdmaEcio']
-            cdma_level = parsed_json['cdmaLevel']
-            dbm = parsed_json['dbm']
-            evdo_dbm = parsed_json['evdoDbm']
-            evdo_ecio = parsed_json['evdoEcio']
-            evdo_level = parsed_json['evdoLevel']
-            evdo_snr = parsed_json['evdoSnr']
-            level = parsed_json['level']
-        except:
-            print 'cdma parse failure'
-            return
-
+    def process_cdma(self, observation_id, parsed_json, task_id, session):
         fresh = Cdma(observation_id)
-        fresh.observation_id = observation_id
-        fresh.register_flag = register_flag
-        fresh.base_station = base_station
-        fresh.latitude = latitude
-        fresh.longitude = longitude
-        fresh.network = network
-        fresh.system = system
-        fresh.asu_level = asu_level
-        fresh.cdma_dbm = cdma_dbm
-        fresh.cdma_ecio = cdma_ecio
-        fresh.cdma_level = cdma_level
-        fresh.dbm = dbm
-        fresh.evdo_dbm = evdo_dbm
-        fresh.evdo_ecio = evdo_ecio
-        fresh.evdo_level = evdo_level
-        fresh.evdo_snr = evdo_snr
-        fresh.level = level
 
-        session.add(fresh)
-        session.commit()
+        try:
+            fresh.register_flag = parsed_json['registerFlag']
+            fresh.base_station = parsed_json['baseStation']
+            fresh.latitude = parsed_json['latitude']
+            fresh.longitude = parsed_json['longitude']
+            fresh.network = parsed_json['network']
+            fresh.system = parsed_json['system']
 
-        self.cdma_loaded += 1
+            fresh.asu_level = parsed_json['asuLevel']
+            fresh.cdma_dbm = parsed_json['cdmaDbm']
+            fresh.cdma_ecio = parsed_json['cdmaEcio']
+            fresh.cdma_level = parsed_json['cdmaLevel']
+            fresh.dbm = parsed_json['dbm']
+            fresh.evdo_dbm = parsed_json['evdoDbm']
+            fresh.evdo_ecio = parsed_json['evdoEcio']
+            fresh.evdo_level = parsed_json['evdoLevel']
+            fresh.evdo_snr = parsed_json['evdoSnr']
+            fresh.level = parsed_json['level']
+
+            session.add(fresh)
+            session.commit()
+
+            self.cdma_loaded += 1
+        except:
+            self.log_writer(6, 'cdma failure', task_id, session)
+            return
 
         return fresh
 
-    def process_gsm(self, observation_id, parsed_json, session):
-        try:
-            register_flag = parsed_json['registerFlag']
-            arfcn = parsed_json['arfcn']
-            bsic = parsed_json['bsic']
-            cid = parsed_json['cid']
-            lac = parsed_json['lac']
-            mcc = parsed_json['mcc']
-            mnc = parsed_json['mnc']
-            asu_level = parsed_json['asuLevel']
-            dbm = parsed_json['dbm']
-            level = parsed_json['level']
-        except:
-            print 'gsm parse failure'
-            return
-
+    def process_gsm(self, observation_id, parsed_json, task_id, session):
         fresh = Gsm(observation_id)
-        fresh.register_flag = register_flag
-        fresh.arfcn = arfcn
-        fresh.bsic = bsic
-        fresh.cid = cid
-        fresh.lac = lac
-        fresh.mcc = mcc
-        fresh.mnc = mnc
-        fresh.asu_level = asu_level
-        fresh.dbm = dbm
-        fresh.level = level
 
-        session.add(fresh)
-        session.commit()
+        try:
+            fresh.register_flag = parsed_json['registerFlag']
+            fresh.arfcn = parsed_json['arfcn']
+            fresh.bsic = parsed_json['bsic']
+            fresh.cid = parsed_json['cid']
+            fresh.lac = parsed_json['lac']
+            fresh.mcc = parsed_json['mcc']
+            fresh.mnc = parsed_json['mnc']
 
-        self.gsm_loaded += 1
+            fresh.asu_level = parsed_json['asuLevel']
+            fresh.dbm = parsed_json['dbm']
+            fresh.level = parsed_json['level']
+
+            session.add(fresh)
+            session.commit()
+
+            self.gsm_loaded += 1
+        except:
+            self.log_writer(6, 'gsm failure', task_id, session)
+            return
 
         return fresh
 
-    def process_lte(self, observation_id, parsed_json, session):
-        try:
-            register_flag = parsed_json['registerFlag']
-            ci = parsed_json['ci']
-            earfcn = parsed_json['earfcn']
-            mcc = parsed_json['mcc']
-            mnc = parsed_json['mnc']
-            pci = parsed_json['pci']
-            tac = parsed_json['tac']
-
-            asu_level = parsed_json['asuLevel']
-            dbm = parsed_json['dbm']
-            level = parsed_json['level']
-            timing_advance = parsed_json['timingAdvance']
-        except:
-            print 'lte parse failure'
-            return
-
+    def process_lte(self, observation_id, parsed_json, task_id, session):
         fresh = Lte(observation_id)
-        fresh.register_flag = register_flag
-        fresh.ci = ci
-        fresh.earfcn = earfcn
-        fresh.mcc = mcc
-        fresh.mnc = mnc
-        fresh.pci = pci
-        fresh.tac = tac
-        fresh.asu_level = asu_level
-        fresh.dbm = dbm
-        fresh.level = level
-        fresh.timing_advance = timing_advance
 
-        session.add(fresh)
-        session.commit()
-
-        self.lte_loaded += 1
-
-        return fresh
-
-    def process_wcdma(self, observation_id, parsed_json, session):
         try:
-            register_flag = parsed_json['registerFlag']
-            cid = parsed_json['cid']
-            lac = parsed_json['lac']
-            mcc = parsed_json['mcc']
-            mnc = parsed_json['mnc']
-            psc = parsed_json['psc']
-            uarfcn = parsed_json['uarfcn']
+            fresh.register_flag = parsed_json['registerFlag']
+            fresh.ci = parsed_json['ci']
+            fresh.earfcn = parsed_json['earfcn']
+            fresh.mcc = parsed_json['mcc']
+            fresh.mnc = parsed_json['mnc']
+            fresh.pci = parsed_json['pci']
+            fresh.tac = parsed_json['tac']
 
-            asu_level = parsed_json['asuLevel']
-            dbm = parsed_json['dbm']
-            level = parsed_json['level']
+            fresh.asu_level = parsed_json['asuLevel']
+            fresh.dbm = parsed_json['dbm']
+            fresh.level = parsed_json['level']
+            fresh.timing_advance = parsed_json['timingAdvance']
+
+            session.add(fresh)
+            session.commit()
+
+            self.lte_loaded += 1
         except:
-            print 'wcdma parse failure'
+            self.log_writer(6, 'lte failure', task_id, session)
             return
 
+        return fresh
+
+    def process_wcdma(self, observation_id, parsed_json, task_id, session):
         fresh = Wcdma(observation_id)
-        fresh.register_flag = register_flag
-        fresh.cid = cid
-        fresh.lac = lac
-        fresh.mcc = mcc
-        fresh.mnc = mnc
-        fresh.psc = psc
-        fresh.uarfcn = uarfcn
-        fresh.asu_level = asu_level
-        fresh.dbm = dbm
-        fresh.level = level
 
-        session.add(fresh)
-        session.commit()
+        try:
+            fresh.register_flag = parsed_json['registerFlag']
+            fresh.cid = parsed_json['cid']
+            fresh.lac = parsed_json['lac']
+            fresh.mcc = parsed_json['mcc']
+            fresh.mnc = parsed_json['mnc']
+            fresh.psc = parsed_json['psc']
+            fresh.uarfcn = parsed_json['uarfcn']
 
-        self.wcdma_loaded += 1
+            fresh.asu_level = parsed_json['asuLevel']
+            fresh.dbm = parsed_json['dbm']
+            fresh.level = parsed_json['level']
+
+            session.add(fresh)
+            session.commit()
+
+            self.wcdma_loaded += 1
+        except:
+            self.log_writer(6, 'wcdma failure', task_id, session)
+            return
 
         return fresh
 
-    def process_cellular(self, observation_id, parsed_json, session):
+    def process_cellular(self, observation_id, parsed_json, task_id, session):
         cell_type = parsed_json['cellType']
         print cell_type
         if cell_type == 'cdma':
-            self.process_cdma(observation_id, parsed_json, session)
+            self.process_cdma(observation_id, parsed_json, task_id, session)
         elif cell_type == 'gsm':
-            self.process_gsm(observation_id, parsed_json, session)
+            self.process_gsm(observation_id, parsed_json, task_id, session)
         elif cell_type == 'lte':
-            self.process_lte(observation_id, parsed_json, session)
+            self.process_lte(observation_id, parsed_json, task_id, session)
         elif cell_type == 'wcdma':
-            self.process_wcdma(observation_id, parsed_json, session)
+            self.process_wcdma(observation_id, parsed_json, task_id, session)
 
     def execute(self, parsed_json, task_id, session):
         installation = self.select_installation(parsed_json['installation'], parsed_json['phone1'], task_id, session)
@@ -295,7 +250,7 @@ class LoaderVersion1:
         self.wifi_loaded = 0
         wifi_list = parsed_json['wiFi']
         for wifi in wifi_list:
-            self.process_wifi(observation.id, wifi, session)
+            self.process_wifi(observation.id, wifi, task_id, session)
 
         self.cdma_loaded = 0
         self.gsm_loaded = 0
@@ -303,7 +258,7 @@ class LoaderVersion1:
         self.wcdma_loaded = 0
         cellular_list = parsed_json['cellular']
         for cellular in cellular_list:
-            self.process_cellular(observation.id, cellular, session)
+            self.process_cellular(observation.id, cellular, task_id, session)
 
         return (observation.id, self.ble_loaded, self.wifi_loaded, self.cdma_loaded, self.gsm_loaded, self.lte_loaded, self.wcdma_loaded)
 
